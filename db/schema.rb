063dab676c34993716438bdf5148685ec4f1e887
a1740098 <a1740098@github.cs.adelaide.edu.au>
2020-08-18 15:12:02 +0930

    migrated subject to course

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_024624) do
+ActiveRecord::Schema.define(version: 2020_08_18_054057) do
 
   create_table "assignments", force: :cascade do |t|
     t.string "title"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_08_18_024624) do
     t.text "descr"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "subject"
   end
 
 end
