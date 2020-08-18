24b5e5ba154935d90ddac5fd96d8e8283b8d368e
a1740098 <a1740098@github.cs.adelaide.edu.au>
2020-08-18 11:47:52 +0930

    Revert "seeding in dataq"
    This reverts commit f7b73e0228b84f1c17a93a0990f23cc1574c1ddd.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-# Seeding in courses
-require 'csv'
-
-csv_text = File.read(Rails.root.join('lib','seeds','CM_CRSE_CAT_ECMS-6383074.csv'))
-csv = CSV.parse(csv_text, :headers => true)
-
-csv.each do |row|
-    c = Course.new
-
-end
-
