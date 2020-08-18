4e0330a53c37139a3ed2de7581f6d0293ba536d4
a1740098 <a1740098@github.cs.adelaide.edu.au>
2020-08-18 12:10:13 +0930

    completed migration for assignment

--- db/migrate/20200818023456_create_assignments.rb
+++ db/migrate/20200818023456_create_assignments.rb
@@ -1,7 +1,10 @@
 class CreateAssignments < ActiveRecord::Migration[5.2]
   def change
     create_table :assignments do |t|
-
+      t.string :title
+      t.date :due_date
+      t.float :weighting
+      t.integer :max_points
       t.timestamps
     end
   end
