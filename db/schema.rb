133ba9346a5030c1ba3b16e5c4cab0cb160243f9
Tyndall Ye <tyndall.ye@gmail.com>
2020-08-19 14:45:01 +0930

    add Student_enrolment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_054057) do
+ActiveRecord::Schema.define(version: 2020_08_19_045221) do
 
   create_table "assignments", force: :cascade do |t|
     t.string "title"
