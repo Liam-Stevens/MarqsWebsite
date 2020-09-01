fd5723fdb765ced5a544f09851fac5f6fd51c140
Jonathon Gaczol <jonathon.gaczol@student.adelaide.edu.au>
2020-09-01 11:14:23 +0000

    UI: Improve student 'show' view

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,19 +1,6 @@
 class StudentsController < ApplicationController
-    def index
-        id = params[:id]
-        @student = Student.find(id)
-    end
-
     def show
-        id = params[:id]
-        @student = Student.find(id)
-        @studentName = @student.full_name
-        @courses = @student.courses
+        @courses = @logged_in_user.courses
         @assignments = @courses[0].assignments
-    end
-
-    def course
-        id = params[:id]
-        @student = Student.find(id)
     end
 end
