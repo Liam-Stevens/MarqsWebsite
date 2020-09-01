d7986931b9442017aad3fa0c51b62b645176dc7b
Jonathon Gaczol <jonathon.gaczol@student.adelaide.edu.au>
2020-09-01 08:17:59 +0000

    New Comments: Implement actual functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,22 @@
 class CommentsController < ApplicationController
-  def create
-    @submission = Submission.find(params[:submission_id])
-    @comment = @submission.comments.create!(params[:comments])
-    redirect_to submissions_path
-  end
+    def create
+        # Find submission with matching ID
+        submission = Submission.find(params[:submission_id])
 
-  def new
-    @submission = Submission.find(params[:submission_id])
-    @comment = @submission.comment.new
-  end
+        # Create a comment and assign to submission
+        comment = Comment.new
+        comment.marker_id = Marker.all[0].id        # Get first marker for now
+        comment.submission_id = submission.id
+        comment.content = params[:text]
+        comment.save!
+
+        # Redirect to submission
+        redirect_to submission_path(submission.id)
+    end
+
+    def new
+        # Find submission with matching ID and get student
+        submission = Submission.find(params[:submission_id])
+        @student = submission.student
+    end
 end
