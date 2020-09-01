ec3ae14ecfcf5c86fae9ff2584ddc2fba9187fdf
Jonathon Gaczol <jonathon.gaczol@student.adelaide.edu.au>
2020-09-01 12:00:51 +0000

    UI: Improve course view

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,9 +1,11 @@
 class CoursesController < ApplicationController
-
     def show
+        # Get course object for ID
         id = params[:id]
         @course = Course.find(id)
-        @assignments = @course.assignments
-    end
 
+        # Get course's assignments and sort by due date
+        @assignments = @course.assignments
+        @assignments.order("due_date DESC")
+    end
 end
--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,5 +1,5 @@
 class Assignment < ApplicationRecord
+    default_scope {order("due_date ASC")}
     belongs_to :course
     has_many :submissions
-    
 end
