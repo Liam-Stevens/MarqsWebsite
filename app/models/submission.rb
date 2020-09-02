e1e65fe5be2e2c006d17d3d6f7b9ec3a8fd7ddf0
a1740098 <a1740098@github.cs.adelaide.edu.au>
2020-09-02 13:45:45 +0930

    added validation to edit grades

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,4 +2,5 @@ class Submission < ApplicationRecord
     belongs_to :assignment
     has_many :comments
     belongs_to :student
+    validates :grade , numericality: {greater_than_or_equal_to: 0}
 end
