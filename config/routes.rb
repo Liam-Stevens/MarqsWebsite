6c0ce7fd24579b244b9574194e3f01e51401829f
Tyndall Ye <tyndall.ye@gmail.com>
2020-09-02 16:57:24 +0930

    fix minor route bugs

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   # Nest submissions within assignments
   # e.g. /assignment/1234/submissions   -> list all submissions for assignment
   # e.g. /submissions/22                -> list specific submission
-  shallow do
-    resources :assignments do
-      resources :submissions, shallow: true
-    end
-  end
+  
+  # shallow do
+  #   resources :assignments do
+  #     resources :submissions, shallow: true
+  #   end
+  # end
   
   root 'login#index'
   resources :login
@@ -22,7 +23,7 @@ Rails.application.routes.draw do
   resources :markers do
     resources :courses do
       resources :assignments do
-        resources :submissions
+        resources :submissions, shallow: true
       end
     end
   end
