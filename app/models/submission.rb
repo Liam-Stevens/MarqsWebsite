c94aa7d336b186527702c307e31ed296fe0b4285
Huy Gia Do Vu <a1742494@student.adelaide.edu.au>
2020-09-10 03:55:38 +0000

    commented out weird breaking application code and also  import marks see to be working

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -6,12 +6,12 @@ class Submission < ApplicationRecord
     belongs_to :student
 
     #Validates that grade is within range
-    validates :grade , numericality: {greater_than_or_equal_to: 0}, allow_nil: true
+    validates :grade , numericality: {greater_than_or_equal_to: 0, message: "Grade can't be negative"}, allow_nil: true
     validate :grade_cannot_be_greater_than_max
 
     def grade_cannot_be_greater_than_max 
         if (!grade.nil? && grade > assignment.max_points) 
-            errors.add(:grade, "Can't be greater than max points: " + assignment.max_points.to_s)
+            errors.add(:grade, student_id.to_s + "'s grade can't be greater than max points: " + assignment.max_points.to_s)
         end
         
     end
