d40a36010a99102aa9a30dfacd1f3f173b1d6154
NamboLambo <nambo@DESKTOP-FI16UJ1.localdomain>
2020-09-15 22:26:28 +0930

    added validation for weights and marks

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -2,4 +2,8 @@ class Assignment < ApplicationRecord
     default_scope {order("due_date ASC")}
     belongs_to :course
     has_many :submissions
+
+    validates :max_points , numericality: {greater_than_or_equal_to: 1, message: "max marks can't be zero"}, allow_nil: false
+    validates :weighting , numericality: {greater_than_or_equal_to: 1, message: "weighting can't be zero"}, allow_nil: false
+
 end
