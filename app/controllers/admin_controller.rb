78eeb8607b1419802d76c797f25bb7d8863629ac
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2020-10-07 17:03:42 +1030

    add student and marker forms completed up to adding courses

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -19,11 +19,23 @@ class AdminController < ApplicationController
       course = Course.new
       course.eff_date = params[:Eff_Date]
       course.course_id = params[:Course_ID]
-      course.short_title = params[:Song_Title]
+      course.short_title = params[:Short_Title]
       course.long_title = params[:Long_Title]
       course.descr = params[:Descr]
       course.subject = params[:Subject]
       course.save!
+    elsif params[:commit] == "Add Student"
+      student = Student.new
+      student.student_id = params[:Student_ID]
+      student.first_name = params[:First_Name]
+      student.last_name = params[:Last_Name]
+      student.save!
+    elsif params[:commit] == "Add Marker"
+      marker = Marker.new
+      marker.marker_id = params[:Marker_ID]
+      marker.first_name = params[:First_Name]
+      marker.last_name = params[:Last_Name]
+      marker.save!
     end
 
     redirect_to admin_index_path
