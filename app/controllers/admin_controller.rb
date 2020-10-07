7426de57c30f3877b0ae1046a0b546bd1c395532
Tyndall Ye <tyndall.ye@gmail.com>
2020-10-07 17:25:20 +1030

    assignment update

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,3 +1,5 @@
+include Errors
+
 class AdminController < ApplicationController
   def index
     add_breadcrumb "Manager", admin_index_path
@@ -12,6 +14,8 @@ class AdminController < ApplicationController
     add_breadcrumb "Manager", admin_index_path
     add_breadcrumb "Add new "+@type, new_admin_path(:type => @type)
 
+    @courses = Course.all
+
   end
 
   def create
@@ -19,13 +23,26 @@ class AdminController < ApplicationController
       course = Course.new
       course.eff_date = params[:Eff_Date]
       course.course_id = params[:Course_ID]
-      course.short_title = params[:Song_Title]
+      course.short_title = params[:Short_Title]
       course.long_title = params[:Long_Title]
       course.descr = params[:Descr]
       course.subject = params[:Subject]
       course.save!
+      redirect_to admin_index_path
+    elsif params[:commit] == "Add Assignment"
+      if (Course.find_by(course_id: params[:Course_id]) != nil)
+        assignment = Assignment.new
+        assignment.course = Course.find(params[:Course_id])
+        assignment.title = params[:Title]
+        assignment.due_date = params[:Date]
+        assignment.weighting = params[:Weighting]
+        assignment.max_points = params[:Max_points]
+        assignment.save!
+        redirect_to admin_index_path
+      else
+        addError("Course not Found")
+        redirect_to new_admin_path(:type => "assignment")
+      end
     end
-
-    redirect_to admin_index_path
   end
 end
